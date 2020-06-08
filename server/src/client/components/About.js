e217c690d948f945eccb13e3aa4c066a4e219c76
Sabrina Bosco <sabrina.bosco@springernature.com>
2020-06-08 16:18:35 +0200

    static router and browser router

---
+++ server/src/client/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <div>I'm the About component</div>
+      <button onClick={() => console.log('Hi there!')}>Press me!</button>
+    </div>
+  );
+};
+
+export default About;
